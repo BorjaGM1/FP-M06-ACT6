9689e4031dd21e0348e7b0fbfd36fe866b51c57a
BorjaGM1 <71267771+BorjaGM1@users.noreply.github.com>
2021-12-21 23:31:59 +0100

    Update

--- r1.php
+++ r1.php
@@ -15,24 +15,6 @@ $c2 = $_POST["c2"];
 $c3 = $_POST["c3"];
 $c4 = $_POST["c4"];
 
-if ($_SERVER["REQUEST_METHOD"]=="POST"){
-    if (empty($_POST["c1"])){
-        $s1er = "value is required";
-    }elseif ($c1 <1){
-        $s1er = "value must be positive";
-    }elseif ($c1 >9){
-        $s1er = "value must be one digit";
-    }
-    else {
-        $s1 = test_input($_POST["c1"]);
-    }
-}
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
 $respuesta = '{"c1":"' . $c1 . '"
 , "c2":"' . $c2 . '"
 , "c3":"' . $c3 . '"
