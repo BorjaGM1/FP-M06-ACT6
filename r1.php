f8ebb1e49824a3a794dd33feae3706789e918406
BorjaGM1 <71267771+BorjaGM1@users.noreply.github.com>
2021-12-21 23:33:08 +0100

    Update

--- r1.php
+++ r1.php
@@ -1,13 +1,6 @@
 <?php
 session_start();
-if (isset($_SESSION["contador"])) {
-    $contador = $_SESSION["contador"];
-} else {$contador = 0;}
-if (is_numeric($contador)) {
-    //es un valor numerico
-}
-$contador++;
-$_SESSION["contador"] = $contador;
+
 $c1 = $_POST["c1"];
 $c2 = $_POST["c2"];
 $c3 = $_POST["c3"];
@@ -16,6 +9,5 @@ $c4 = $_POST["c4"];
 $respuesta = '{"c1":"' . $c1 . '"
 , "c2":"' . $c2 . '"
 , "c3":"' . $c3 . '"
-, "c4":"' . $c4 . '"
-, "s1er":"' . $s1er . '"}';
+, "c4":"' . $c4 . '"}';
 echo $respuesta;
